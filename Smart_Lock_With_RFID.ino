85275213a0d26332751859ab92f8034f00467b7c
unknown <martin.9494@gmail.com>
2020-01-16 15:02:26 +0100

    GET/POST requests to the DB

--- Smart_Lock_With_RFID.ino
+++ Smart_Lock_With_RFID.ino
@@ -1,9 +1,70 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP8266WiFi.h>        // Include the Wi-Fi library
+#include <ESP8266HTTPClient.h> 
 
+const char* ssid     = "Redmi";         // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "12345678";     // The password of the Wi-Fi network
+
+void setup() {
+  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println('\n');
+  
+  WiFi.begin(ssid, password);             // Connect to the network
+  Serial.print("Connecting to ");
+  Serial.print(ssid); Serial.println(" ...");
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(1000);
+    Serial.print(++i); Serial.print(' ');
+  }
+
+  Serial.println('\n');
+  Serial.println("Connection established!");  
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
+
+
+  String result = Get("http://rfidlock.azurewebsites.net/data.php?");
+  Serial.println("result");
+  Serial.println(result);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() {}
 
+
+String Post(String query) {
+  Serial.println("query is " + query);
+  
+  HTTPClient http;
+  http.begin(query);
+  int httpCode = http.POST(query);
+  
+  String result = "";
+  if (httpCode == 200) {
+      Serial.println("POST success");
+      result = http.getString();
+  } else {
+      Serial.println("ERROR with httpcode: " + httpCode);
+  }
+  http.end();
+  return result;
+}
+
+String Get(String query) {
+  Serial.println("query is " + query);
+  
+  HTTPClient http;
+  http.begin(query);
+  int httpCode = http.GET();
+  
+  String result = "";
+  if (httpCode == 200) {
+      Serial.println("GET success");
+      result = http.getString();
+  } else {
+      Serial.println("ERROR with httpcode: " + httpCode);
+  }
+  http.end();
+  return result;
 }
