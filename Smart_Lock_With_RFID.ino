2be50eb64b74de35bb65d11c1d02780f7fdbfc33
unknown <martin.9494@gmail.com>
2020-01-17 14:31:40 +0100

    RFID Reader - tag reading implementation

--- Smart_Lock_With_RFID.ino
+++ Smart_Lock_With_RFID.ino
@@ -1,17 +1,30 @@
+#include <SPI.h>
+#include <MFRC522.h>
 #include <ESP8266WiFi.h>        // Include the Wi-Fi library
 #include <ESP8266HTTPClient.h> 
 
-const char* ssid     = "Redmi";         // The SSID (name) of the Wi-Fi network you want to connect to
-const char* password = "12345678";      // The password of the Wi-Fi network
-const int records = 1;                  // 0 = Get all records from the DB. 1 = Get only TagUID, AccessLevel.
+#define SS_PIN D8       //RFID pins (ESP8266)
+#define RST_PIN D3
+
+// WiFi connection
+const char* ssid     = "Redmi";           // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "12345678";        // The password of the Wi-Fi network
+//const int records = 1;                  // 0 = Get all records from the DB. 1 = Get only TagUID, AccessLevel.
+
+// RFID Reader initialization
+MFRC522 rfid(SS_PIN, RST_PIN);            // Instance of the class
+byte nuidPICC[4];                         // Init array that will store new NUID 
 
 
 void setup() {
-  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  Serial.begin(9600);         // Start the Serial communication to send messages to the computer
+  SPI.begin();                // Init SPI bus
+  rfid.PCD_Init();            // Init MFRC522 
   delay(10);
+  
   Serial.println('\n');
   
-  WiFi.begin(ssid, password);             // Connect to the network
+  WiFi.begin(ssid, password);                 // Connect to the network
   Serial.print("Connecting to ");
   Serial.print(ssid); Serial.println(" ...");
 
@@ -26,15 +39,64 @@ void setup() {
   Serial.print("IP address:\t");
   Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
 
-  String getQuery = "http://rfidlock.azurewebsites.net/data.php?records=";
-  getQuery = getQuery + records;
-  String result = Get(getQuery); // 
-  Serial.println("Result: ");
-  Serial.println(result);
+
 
 }
 
-void loop() {}
+void loop() {
+  
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if ( ! rfid.PICC_IsNewCardPresent())
+    return;
+
+  // Verify if the NUID has been readed
+  if ( ! rfid.PICC_ReadCardSerial())
+    return;
+
+  // Store NUID into nuidPICC array
+  for (byte i = 0; i < 4; i++) {
+    nuidPICC[i] = rfid.uid.uidByte[i];
+  }
+
+  // Serial printing for debugging/additional info
+  Serial.println(F("The NUID tag is:"));
+  Serial.print(F("In orig: "));
+  for (byte i = 0; i < 4; i++) {
+    Serial.print(rfid.uid.uidByte[i]);
+    Serial.print(" ");
+  }
+  Serial.println("");
+  Serial.print(F("In hex: "));
+  printHex(rfid.uid.uidByte, rfid.uid.size);
+  Serial.println();
+  Serial.print(F("In dec: "));
+  printDec(rfid.uid.uidByte, rfid.uid.size);
+  Serial.println();
+  Serial.println();
+
+  // Construction of a query (for GET request).
+  String getQuery = "http://rfidlock.azurewebsites.net/data.php?tagUID='";
+  for(int i = 0; i<4;i++){
+    getQuery.concat(nuidPICC[i]);         // Concatenates URL with tagUID received from reader (in byte[]).
+  }
+  getQuery.concat("'");
+  
+  Serial.print("Created query: ");
+  Serial.println(getQuery);
+  
+  String result = Get(getQuery);          // Sends GET query to the webserver
+  Serial.print("Result: ");
+  Serial.println(result);
+
+
+  // Halt PICC
+  rfid.PICC_HaltA();
+
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
+  
+  
+  }
 
 
 String Post(String query) {
@@ -71,4 +133,24 @@ String Get(String query) {
   }
   http.end();
   return result;
+}
+
+/**
+ * Helper routine to dump a byte array as hex values to Serial. 
+ */
+void printHex(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+}
+
+/**
+ * Helper routine to dump a byte array as dec values to Serial.
+ */
+void printDec(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], DEC);
+  }
 }
